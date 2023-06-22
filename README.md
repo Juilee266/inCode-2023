c94588410d2a04ae6105beba9d845c5297b285c9
Juilee266 <66289832+Juilee266@users.noreply.github.com>
2023-06-22 09:51:59 +0530

    created hello world proj

--- README.md
+++
@@ -1,2 +0,0 @@
-# inCode-2023
-Prototype developed for 'inCode 2023 - Challenging Injustice' hackathon hosted by HackerEarth
---
+++ app/src/main/java/com/inDrive/plugin/voice/MainActivity.java
@@ -0,0 +1,15 @@
+package com.inDrive.plugin.voice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.inDrive.plugin.voice.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
